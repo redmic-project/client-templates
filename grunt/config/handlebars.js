f058d161b8c073f64f04dbbd568938ff351c4f15
CarlosGC1989 <carlos@redmic.es>
2018-07-26 11:28:55 +0100

    Añade helpers para título de detalles

--- grunt/config/handlebars.js
+++ grunt/config/handlebars.js
@@ -7,6 +7,7 @@ module.exports = function(grunt) {
 				amd: ['templates'],
 				compilerOptions: {
 					knownHelpers: {
+						DetailsTitle: true,
 						PropertiesAtlas: true,
 						StartEnd: true,
 						TimeCumulative: true,
