78f4b922d232247cb06c40d0c6a13f4de279f2ae
Pedro Trujillo <pedroetb@gmail.com>
2022-01-17 09:38:07 +0000

    Elimina parámetro dpi de leyenda, provoca fallos

--- src/Helpers/customParser.js
+++ src/Helpers/customParser.js
@@ -108,7 +108,7 @@ define([
 
 			var imgClass = 'detailsPhoto',
 				noImgUrl = '/resources/images/noIMG.png',
-				legendOptsParam = 'legend_options=fontAntiAliasing:true;dpi:100',
+				legendOptsParam = 'legend_options=fontAntiAliasing:true',
 				imgUrl;
 
 			if (!layer || !(layer.legend || layer.stylesLayer)) {
